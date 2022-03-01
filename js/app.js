9237dbdf6140299fd07ebcbe1fdf5c66a6b7ffae
Iqbal <iqbal.du.mis@gmail.com>
2022-03-01 18:18:50 +0600

    20 search result display fixed

--- js/app.js
+++ js/app.js
@@ -35,16 +35,17 @@ const searchMobile = () => {
 // display search results
 
 const displayMobiles = mobiles => {
+    const twentyMobile = mobiles.slice(0, 20);
     const mobilesData = document.getElementById('mobileDiv');
     const errorMessage2 = document.getElementById('error-message2');
     const mobileDetail = document.getElementById('mobile-detail');
     mobileDetail.textContent = '';
     mobilesData.textContent = '';
-    if (mobiles == false) {
+    if (twentyMobile == false) {
         errorMessage2.style.display = 'block';
         toggleSpinner('none');
     } else {
-        mobiles.forEach(mobile => {
+        twentyMobile.forEach(mobile => {
             const div = document.createElement('div');
             div.className = 'col-lg-4 mb-5';
             div.innerHTML = `
@@ -83,7 +84,7 @@ const displayMobileDetail = mobile => {
         <img src="${mobile.image}" class="mx-auto my-5 w-30 rounded" alt="...">
         <div class="card-body">
         <p class="card-text">Name: ${mobile.name}</p>
-        <p class="card-text">Release Date: ${mobile.releaseDate ? mobile.releaseDate : 'No release date found'}</p>
+        <p class="card-text">Release Date: ${mobile.releaseDate ? mobile.releaseDate : 'Not Available'}</p>
         <p class="card-text">Brand: ${mobile.brand}</p>
         <p class="card-text fs-5 "><p class="fw-bold">Main Fetures</p><p class="fs-6">Storage: ${mobile.mainFeatures.storage}<br> Display: ${mobile.mainFeatures.displaySize}<br> Chipset: ${mobile.mainFeatures.chipSet}<br> Memory: ${mobile.mainFeatures.memory}<br> Sensors: ${mobile.mainFeatures.sensors.join(', ')}</p></p >
     <p class="card-text fs-5 "><p class="fw-bold">Others</p><p class="fs-6">NFC: ${mobile.others?.NFC ? mobile.others?.NFC : 'N/A'}<br>Radio: ${mobile.others?.Radio ? mobile.others?.Radio : 'N/A'}<br>Bluetooth: ${mobile.others?.Bluetooth ? mobile.others?.Bluetooth : 'N/A'}<br>USB: ${mobile.others?.USB ? mobile.others?.USB : 'N/A'}<br>GPS: ${mobile.others?.GPS ? mobile.others?.GPS : 'N/A'}<br>WLAN: ${mobile.others?.WLAN ? mobile.others?.WLAN : 'N/A'}</p></p>
